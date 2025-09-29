d81142d21668845e0f0394514cf52199a4f4c9e3
sandeshpd <sandesh@sandesh.com>
2025-09-29 16:06:02 +0530

    Error handling middleware

---
+++ backend/middleware/error.js
@@ -0,0 +1,12 @@
+function errorHandler(err, req, res, next) {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Errors.";
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+};
+
+export default errorHandler;
