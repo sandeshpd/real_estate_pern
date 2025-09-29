d3c4764fa1a9877055742112fce8f4f107ef75ed
sandeshpd <sandesh@sandesh.com>
2025-09-29 16:13:54 +0530

    Property Listing routes

---
+++ backend/routes/listing.route.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+    getAllListings,
+    getListings,
+    createListing,
+    updateListing,
+    deleteListing,
+    getPublicPropertyById
+} from "../controllers/listing.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/get/all", verifyToken, getAllListings);
+router.post("/create", verifyToken, createListing);
+router.put("/update/:id", verifyToken, updateListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.get("/get/:id", verifyToken, getListings);
+
+// Read only endpoint for any user
+router.get("/get/public/:id", getPublicPropertyById);
+
+export default router;
