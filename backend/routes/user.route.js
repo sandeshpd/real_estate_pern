84698d11b22b53291444358fa778757a36ee0272
sandeshpd <sandesh@sandesh.com>
2025-10-04 16:26:23 +0530

    Added getUserById endpoint

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getUsers, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { getUsers, getUserById, updateUser, deleteUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", verifyToken, getUserById);
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
